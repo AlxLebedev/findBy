b9242e8a903bc527b02c9f7130df0f32a793b3c7
Alexey Lebedev <vorresti@ya.ru>
2020-02-18 15:21:11 +0300

    tests after eslint

--- src/js/__tests__/func.test.js
+++ src/js/__tests__/func.test.js
@@ -10,19 +10,19 @@ test('find by name', () => {
 });
 
 test('find by type', () => {
-    const recieved = data.filter(findBy('type', 'attack'));
-    const expected = [{ name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' }];
-    expect(recieved).toEqual(expected);
+  const recieved = data.filter(findBy('type', 'attack'));
+  const expected = [{ name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' }];
+  expect(recieved).toEqual(expected);
 });
 
 test('find by unexisted keys', () => {
-    const recieved = data.filter(findBy('ola-la', 'bang-bang'));
-    const expected = [];
-    expect(recieved).toEqual(expected);
+  const recieved = data.filter(findBy('ola-la', 'bang-bang'));
+  const expected = [];
+  expect(recieved).toEqual(expected);
 });
 
 test('find without arguments', () => {
-    const recieved = data.filter(findBy());
-    const expected = data;
-    expect(recieved).toEqual(expected);
+  const recieved = data.filter(findBy());
+  const expected = data;
+  expect(recieved).toEqual(expected);
 });
